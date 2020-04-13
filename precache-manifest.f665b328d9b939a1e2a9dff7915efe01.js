80c709f759ac0b35977684ebcb8a096852afd3d3
Suvarna Mondal <suvarnamondal27@gmail.com>
2020-04-13 01:13:57 +0100

    Updates

---
+++ precache-manifest.f665b328d9b939a1e2a9dff7915efe01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64b8ff42d5773fccc07eb2fdc54380d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ab982775a92f1475b85",
+    "url": "/static/css/main.644d83ef.chunk.css"
+  },
+  {
+    "revision": "ab3b81a352883c38226b",
+    "url": "/static/js/2.7f740fb9.chunk.js"
+  },
+  {
+    "revision": "efb64ce026d34ca2f5ada96148e75954",
+    "url": "/static/js/2.7f740fb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ab982775a92f1475b85",
+    "url": "/static/js/main.9e34d3a2.chunk.js"
+  },
+  {
+    "revision": "1e16879ac5ed627b903d",
+    "url": "/static/js/runtime-main.f2550dde.js"
+  },
+  {
+    "revision": "a8b203f30b945ba9de69397142caa060",
+    "url": "/static/media/profile.a8b203f3.png"
+  }
+]);

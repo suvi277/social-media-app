e7c106db5368726d5e283c7d5d2e725628044b7e
Suvarna Mondal <suvarnamondal27@gmail.com>
2020-04-14 01:14:07 +0100

    Updates

---
+++ precache-manifest.51a08defcb2401bea25679694ab513b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e131516a113254a74b3f40596657a61c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62733379efe3fcdd4de2",
+    "url": "/static/css/main.644d83ef.chunk.css"
+  },
+  {
+    "revision": "ff73307198106f729ac2",
+    "url": "/static/js/2.aa78da66.chunk.js"
+  },
+  {
+    "revision": "efb64ce026d34ca2f5ada96148e75954",
+    "url": "/static/js/2.aa78da66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62733379efe3fcdd4de2",
+    "url": "/static/js/main.0d681f75.chunk.js"
+  },
+  {
+    "revision": "1e16879ac5ed627b903d",
+    "url": "/static/js/runtime-main.f2550dde.js"
+  },
+  {
+    "revision": "a8b203f30b945ba9de69397142caa060",
+    "url": "/static/media/profile.a8b203f3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social-media-app/precache-manifest.bf52bb712f93b6d20fbfd7b53b45469e.js"
+  "/precache-manifest.51a08defcb2401bea25679694ab513b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/social-media-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
